05fd82e4758a90da3051d824b425bcaf9b02b553
giastra <gcstracquadanio@gmail.com>
2025-10-13 15:59:47 +0100

    realização dos exercicios 1 ao 5 da ficha 2 em aula AED

---
+++ AED/ficha02/ex02.py
@@ -0,0 +1,78 @@
+
+import random
+#uma central para os exercicios da ficha 02
+print('Uma central de todas as atividades do exercicio 2\n 1-Fatorial\n 2-Soma de numeros pares\n3-Jogo do numero escondido\n4-Verifica se o numero é primo ')
+while True:
+    NAtividade=int(input('\nIndique o numero da atividade que deseja visualizar: '))
+    if NAtividade == 1:
+        Numero=int(input('\nIndique o numero inteiro para a fatorial: '))
+        Fator=1
+        n=0
+        while n < Numero:
+            if Numero == 0:
+                break
+            Fator=(Numero-n)*Fator
+            n+=1
+        print(f'\n{Numero}! = {Fator}')    
+        break
+    elif NAtividade == 2:
+        while True:
+            NInferior=int(input('\nIndique um numero inteiro: '))
+            NSuperior=int(input('\nIndique um numero inteiro maior que o anterior: '))
+            n=NInferior
+            Soma=0
+            if NSuperior < NInferior:
+                print('\nNumeros não validos introduza novamente')
+                continue
+            while n<=NSuperior:
+                if n%2==0:
+                    Soma+=n
+                n+=1
+            print(f'\nA soma de todos os numeros pares entre {NInferior} e {NSuperior} é {Soma}')
+            break
+        break
+    elif NAtividade == 3:
+        while True:   
+            Numero = random.randint(1,50)
+            print('\nBem vindo ao jogo das adivinhações, basta acertar o numero escondido entre 1 a 50, você tem 10 tentativas para acertar')
+            NTentativas=1
+            while NTentativas <=10:
+                NInserido=int(input(f'\nIndique a sua {NTentativas}º escolha: '))
+                if NInserido == Numero:
+                    print(f'\nVoce conseguiu !!!, achou o numero escondido em {NTentativas} tentativas')
+                    break
+                elif NInserido > Numero:
+                    print('O numero escondido é menor')
+                elif NInserido < Numero:
+                    print ('O numero escondido é maior')
+                NTentativas+=1
+            if NInserido != Numero:
+                print (f'\nVoce perdeu :( o numero escondido era {Numero})')
+            Tentar=str(input('\nVoce quer jogar novamente (s/n): '))
+            if Tentar == 'n':
+                break
+        break
+    elif NAtividade == 4:
+        Numero=int(input('\nIntroduza o numero que você quer testar se é primo ou não: '))
+        if Numero %2 ==0 and Numero != 2:
+            print('Ele não é primo')
+        elif Numero %3 ==0 and Numero != 3:
+            print('Ele não é primo')
+        elif Numero %5 ==0 and Numero != 5:
+            print('Ele não é primo')
+        elif Numero %11 ==0 and Numero != 11:
+            print('Ele não é primo')
+        else:
+            print('ele é primo')
+        break
+    elif NAtividade == 5:
+        Numero=int(input('Escolha quantos numeros de .... você quer ver: '))
+        n=1
+        lista=int([0,1])
+        while n !=  Numero:
+            print(lista)
+
+            
+        break
+    else:
+        print('\n - Numero não valido, tente novamente.')
