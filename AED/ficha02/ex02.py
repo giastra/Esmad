1cd3594336620caed965afe9edfda690e1149384
giastra <gcstracquadanio@gmail.com>
2025-10-16 12:15:03 +0100

    mudanças do dia off e da aula de AED

--- AED/ficha02/ex02.py
+++ AED/ficha02/ex02.py
@@ -1,7 +1,7 @@
 
 import random
 #uma central para os exercicios da ficha 02
-print('Uma central de todas as atividades do exercicio 2\n 1-Fatorial\n 2-Soma de numeros pares\n3-Jogo do numero escondido\n4-Verifica se o numero é primo ')
+print('Uma central de todas as atividades do exercicio 2\n 1-Fatorial\n 2-Soma de numeros pares\n3-Jogo do numero escondido\n4-Verifica se o numero é primo\n5-Sequência de Fibonacci\n6-Numero perfeito\n7-calculadora de binario ')
 while True:
     NAtividade=int(input('\nIndique o numero da atividade que deseja visualizar: '))
     if NAtividade == 1:
@@ -68,11 +68,54 @@ while True:
     elif NAtividade == 5:
         Numero=int(input('Escolha quantos numeros de .... você quer ver: '))
         n=1
-        lista=int([0,1])
-        while n !=  Numero:
-            print(lista)
-
-            
+        print('| 0 | ',end='')
+        while n <  Numero:
+            Formula= (1/pow( 5,0.5))*(pow((1+pow(5,0.5))/2,n)-pow((1-pow(5,0.5))/2,n))
+            print('| {:.0f} | '.format(Formula),end="")
+            n+=1
+        break
+    elif NAtividade == 6:
+        Numero=int(input('Introduza um numero positivo interiro: '))
+        n=2
+        e=1
+        print('a soma dos divizores é : 1',end='')
+        while n<Numero:
+            if Numero%n ==0:
+                e=e+n
+                print(f' + {n}',end='')
+            n+=1
+        print(f' = {e}')
+        if e == Numero:
+            print (' Logo é perfeito')
+        else:
+            print(' Logo não é perfeito')     
+        break
+    elif NAtividade==7:
+        q=int(input('um numero '))
+        b=('')
+        while q >= 1:
+            r=abs(q%2)
+            if r >0:
+                d=('1')
+            else:
+                d=('0')
+            b = (d + ' ' + b)
+            print(r)
+            q=q/2
+            print(q)
+        print(b)
+        break
+    elif NAtividade == 8:
+        Numero=int(input('Quantidade de numeros: '))
+        e = 0
+        f = 1
+        while Numero > 0:
+            n = float(input('introduza um numero: '))
+            if n > f:
+                e=f
+                f=n
+            Numero -=1
+        print('O segundo maior numer é: ',e)
         break
     else:
         print('\n - Numero não valido, tente novamente.')
