a4d5efc7b7af7b2078f0f5aa52ce61f234b5bc2d
giastra <giastra>
2025-11-20 12:46:26 +0000

    aed, ficha 07, fim do exercicio 01 e inico do 02

--- AED/ficha07/ex07.py
+++ AED/ficha07/ex07.py
@@ -1,9 +1,8 @@
 def AddContry(fileName):
     with open(fileName, 'r+' , encoding='utf-8') as file:
-        pais = str(input('Idroduza um pais '))
-        continente= str(input('Indroduza o continente desse pais '))
-        print(file.read())
-        lista=(file.read())
+        pais = str(input('Introduza um país '))
+        continente= str(input('Introduza o continente desse país '))
+        lista=file.read()
         lista=lista.split(',')
         print(lista)
         if lista.count(pais)==0:
@@ -12,13 +11,28 @@ def AddContry(fileName):
             print('Esse pais já existe na lista')
     file.close()
 
-
+def showCountries(fileName):
+    with open(fileName , 'r', encoding='utf-8') as file:    
+        lista=file.read()
+        lista=lista.split(',')
+    n=0
+    for x in range(len(lista)-1):
+        if x%2==0:
+            print('\n\t',lista[x],end=' ')
+        else:
+            print(lista[x])
+        
 pasta = 'files'
 fichero = 'países.txt'
-with open(fichero, 'a' , encoding='utf-8') as file:
-    AddContry(fichero)
     
-file.close()
-with open(fichero , 'r', encoding='utf-8') as file:    
-    print(file.read())
-file.close()
+while True:
+    print('MENU\n1-\n2-\n3-\n4-\n0-')
+    intro = int(input('\topção: '))
+    if intro == 1:
+        AddContry(fichero)
+    elif intro == 2:
+        showCountries(fichero)
+    elif intro == 0:
+        break
+    else:
+        print('\nOpção não valida, tente novamente')   
