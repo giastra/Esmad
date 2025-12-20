ceb23875afbf5f2bd81164d9544892b45323b0ea
giastra <giastra>
2025-12-20 14:09:37 +0000

    ficha 04 de dom tw

--- TW/DOM_FIcha04/script.js
+++ TW/DOM_FIcha04/script.js
@@ -6,21 +6,38 @@ let tasks = [
     category: "chores"},
 ]
 
-tasks[0].task
-document.getElementById('list').innerHTML+=`<div class="container text-center">`
+function lista(){
+document.getElementById('list').innerHTML=`<div class="container text-center">`
 for (de=0;de<tasks.length;de++)
     {
     document.getElementById('list').innerHTML+=`
     <div class="col w-100 mw-100 text-center">
-    <button type="button" class="btn btn-success">mark as complete</button> 
-    ${tasks[de].task}......................${tasks[de].category}
+    <button type="button" class="btn btn-success" onclick="ccd(${de})" data-bs-toggle="modal" data-bs-target="#registerModal">mark as complete</button> 
+     ${tasks[de].task}......................${tasks[de].category}
     </div>
     </div>`
 }
+}
 
+lista()
 document.getElementById('buton').addEventListener("click",MoreTask)
 function MoreTask(event){
     event.preventDefault()
-    let txt = document.getElementById("task").innerHTML
-    alert(txt)
+    let txt = document.getElementById("task").value
+    let cat = document.getElementById("category").value
+    tasks.push({task:txt,category:cat})
+    lista()
 }
+let numero = 0
+function ccd(f){
+    numero=f
+}
+
+function sim(ded){
+    if (ded == true){
+            tasks.splice(numero, 1) 
+            lista()
+    } 
+}
+
+
