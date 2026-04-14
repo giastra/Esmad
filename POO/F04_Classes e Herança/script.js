ddfb702d2a2b23ec2f1be576cb73bcee22a9c63d
giastra <giastra>
2026-04-14 14:12:40 +0100

    poo, inicio da att 4

---
+++ POO/F04_Classes e Herança/script.js
@@ -0,0 +1,25 @@
+// class Track {
+//     title = ''
+//     artist = ''
+//     duration = ''
+//     genre = ''
+//     plays = 0
+//     liked = false
+//     constructor(title, artist, duration,genre) {
+//         this.title=title;
+//         this.artist=artist;
+//         this.duration=duration;
+//         this.genre=genre;
+//     }
+//     /**
+//      * 
+//      */
+//     name() {
+        
+//     }
+
+// }
+// let newTrack=new Track('juan','top','20','rock')
+// console.log(newTrack.title);
+
+console.log(document.querySelectorAll(".film-card"));
